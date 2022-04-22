0d0076e6df1a087c95e9541cf1b2ba8d334b946f
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-22 14:00:32 -0500

    add: third requirement test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,7 @@
 import User from './../../app/models/User'
 
 describe("Unit test for user class", () => {
-    test("Create an User object", () => {
+    test("1) Create an User object", () => {
         //Instance a User
         const testUser = new User(123,'@JohnDoe','John Doe', 'Bio', 'dateCreated', 'lastUpdate');
         //Test validation
@@ -12,12 +12,23 @@ describe("Unit test for user class", () => {
         expect(testUser.dateCreated).toBe('dateCreated')
         expect(testUser.lastUpdate).toBe('lastUpdate')
     });
-    test("Validation of dateCreated and lastUpdate", () => {
-        let testDate = new Date();
+    test("2) Validation of dateCreated and lastUpdate", () => {
         //Instance a user
-        const testUser = new User(222,'@JohnDoe','John Doe', 'Bio', testDate, testDate);
+        const testUser = new User(222,'@JohnDoe','John Doe', 'Bio');
         //Test validation
-        expect(testUser.dateCreated).toBe(testDate)
-        expect(testUser.lastUpdate).toBe(testDate)
+        expect(testUser.dateCreated).not.toBeUndefined()
+        expect(testUser.lastUpdate).not.toBeUndefined()
+    });
+    test("3) Test getters", () => {
+        //Instance a user
+        const testUser = new User(222,'@JohnDoe','John Doe', 'Bio');
+        //Test getters
+        expect(user.getUsername).toBe('@JohnDoe')
+        expect(user.getBio).toBe('Bio')
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    });
+    test("4) Test setters", () => {
+
     });
 });
