18bbcad170d426a4a60d3399297879ca85ecdd2d
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-22 14:29:51 -0500

    fix: property name or User class

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,32 +3,37 @@ import User from './../../app/models/User'
 describe("Unit test for user class", () => {
     test("1) Create an User object", () => {
         //Instance a User
-        const testUser = new User(123,'@JohnDoe','John Doe', 'Bio', 'dateCreated', 'lastUpdate');
+/*         const testUser = new User(123,'@JohnDoe','John Doe', 'Bio', 'dateCreated', 'lastUpdate'); */
+        const testUser = new User(123,'@JohnDoe','John Doe', 'Bio');
         //Test validation
         expect(testUser.id).toBe(123)
         expect(testUser.username).toBe('@JohnDoe')
         expect(testUser.name).toBe('John Doe')
         expect(testUser.bio).toBe('Bio')
-        expect(testUser.dateCreated).toBe('dateCreated')
-        expect(testUser.lastUpdate).toBe('lastUpdate')
+        /* expect(testUser.dateCreated).toBe('dateCreated')
+        expect(testUser.lastUpdated).toBe('lastUpdate') */
     });
     test("2) Validation of dateCreated and lastUpdate", () => {
         //Instance a user
         const testUser = new User(222,'@JohnDoe','John Doe', 'Bio');
         //Test validation
         expect(testUser.dateCreated).not.toBeUndefined()
-        expect(testUser.lastUpdate).not.toBeUndefined()
+        expect(testUser.lastUpdated).not.toBeUndefined()
     });
     test("3) Test getters", () => {
         //Instance a user
         const testUser = new User(222,'@JohnDoe','John Doe', 'Bio');
         //Test getters
-        expect(user.getUsername).toBe('@JohnDoe')
-        expect(user.getBio).toBe('Bio')
-        expect(user.getDateCreated).not.toBeUndefined()
-        expect(user.getLastUpdated).not.toBeUndefined()
+        expect(testUser.getUsername).toBe('@JohnDoe')
+        expect(testUser.getBio).toBe('Bio')
+        expect(testUser.getDateCreated).not.toBeUndefined()
+        expect(testUser.getLastUpdated).not.toBeUndefined()
     });
     test("4) Test setters", () => {
-
+        //Instance a User
+        const testUser = new User(222,'@JohnDoe','John Doe', 'Bio');
+        //Test setters
+        expect(testUser.setUserName('newUser')).toBe('newUser')
+        expect(testUser.setBio('newBio')).toBe('newBio')
     });
 });
